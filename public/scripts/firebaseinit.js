442b0a496727ebdc641a16d45691f334d74dc410
Davi Batista Tabosa <376851@UFCVIRTUAL.LOCAL>
2017-10-17 17:20:16 -0300

    pedidos acesso

---
+++ public/scripts/firebaseinit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAJQSF4mCK-_t4jFOIFf_q012nlNyY_du0",
+  authDomain: "smdevasao.firebaseapp.com",
+  databaseURL: "https://smdevasao.firebaseio.com",
+  projectId: "smdevasao",
+  storageBucket: "smdevasao.appspot.com",
+  messagingSenderId: "1078082262613"
+};
+firebase.initializeApp(config);
