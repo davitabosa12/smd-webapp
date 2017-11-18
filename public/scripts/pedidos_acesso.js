d9dbb14e784c264a51eef4b2c5b46e4751af735b
Davi Tabosa <davitabosa12@gmail.com>
2017-11-18 20:49:45 -0300

    testes

--- public/scripts/pedidos_acesso.js
+++ public/scripts/pedidos_acesso.js
@@ -1,11 +1,31 @@
 var database = firebase.database();
 var ss;
+class Mock {
+        constructor(nome,mat,cpf){
+            this.nome = nome;
+            this.matricula = mat;
+            this.cpf = cpf;
+        }
+        child(property){
+            return this[property];
+        }
+
+    }
+
 var pedidoRef = database.ref("pedidoIngresso");
 window.onload = function(){
     var tabela = document.getElementById("tabela_corpo");
-    pedidoRef.orderByKey().limitToFirst(20).on("child_added", snap =>{
+    /*pedidoRef.orderByKey().limitToFirst(20).on("child_added", snap =>{
         renderLinha(tabela,snap);
-    });
+    });*/
+
+    //mockup data
+    var data = [];
+    for(var i = 0; i < 20; i++){
+        var n = new Mock(faker.name.findName(),faker.random.number(),faker.random.number());
+        renderLinha(tabela,n);
+    }
+    
 }
 /**
  * 
@@ -14,8 +34,10 @@ window.onload = function(){
  */
 function renderLinha(tabela,dados) {
     var row = tabela.insertRow();
-    var matricula = row.insertCell(0); //matricula
-    var nome = row.insertCell(1); //nome
+    var nome = row.insertCell(0); //nome
+    nome.classList.add("mdl-data-table__cell--non-numeric");
+    var matricula = row.insertCell(1); //matricula
+    
     var cpf = row.insertCell(2); //cpf
 
     
@@ -24,8 +46,8 @@ function renderLinha(tabela,dados) {
     nome.innerHTML = dados.child("nome").val();
     cpf.innerHTML = dados.child("cpf").val();
 
-    adicionarBotoes(row,()=>{alert("Matricula" + dados.child("matricula").val() + "deferida.")},
-                        ()=>{alert("Matricula" + dados.child("matricula").val() + "indeferida.")});
+    adicionarBotoes(row,()=>{console.log("Matricula" + dados.child("matricula").val() + "deferida.")},
+                        ()=>{console.log("Matricula" + dados.child("matricula").val() + "indeferida.")});
 
 }
 /**
@@ -40,9 +62,11 @@ function adicionarBotoes(row, accept, reject){
 
     btnDeferir.innerHTML = "Deferir";
     btnDeferir.addEventListener("click",accept);
+    btnDeferir.className += "mdl-button mdl-js-button mdl-js-ripple-effect";
     row.insertCell(3).appendChild(btnDeferir);
 
     btnIndeferir.innerHTML = "Indeferir";
     btnIndeferir.addEventListener("click",reject);
+    btnIndeferir.className += "mdl-button mdl-js-button mdl-js-ripple-effect";
     row.insertCell(4).appendChild(btnIndeferir);
 }
