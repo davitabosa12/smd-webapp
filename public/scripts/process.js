adfb5a753f881835e77c98089be24d0fcd906a32
Davi Tabosa <davitabosa12@gmail.com>
2017-11-24 19:41:18 -0300

    malhorar noticias

--- public/scripts/process.js
+++ public/scripts/process.js
@@ -8,6 +8,11 @@ var rootNoticia;
         var titulo = document.getElementsByName("titulo")[0].value;
         var desc = document.getElementsByName("descricao")[0].value;
         var tipo = document.getElementsByName("tipo")[0].value;
+        if(titulo === undefined || titulo === ""||
+             desc === undefined ||   desc === ""){
+            Materialize.toast("Preencha todos os campos.",5000);
+            return;
+        }
         database = firebase.database();
         console.log(firebase);
         console.log(database);
@@ -18,7 +23,33 @@ var rootNoticia;
             tipo: tipo
         };
         
-        var ss = rootNoticia.push(newNoticia);
+        var ss = rootNoticia.push(newNoticia, error=>{
+            if(!error){
+                Materialize.toast("Mensagem enviada com sucesso",3000,"rounded");
+            }
+            else{
+                var msg = "Houve um erro: " + error;
+                Materialize.toast(msg, 5000,"rounded")
+            }
+        });
         console.log(ss);
 
     }
+    var connectedRef = firebase.database().ref(".info/connected");
+    setTimeout(() =>{
+        var connectionLostOnce = false;
+        connectedRef.on("value", function(snap) {
+            
+        if (snap.val() === true) {
+            if(connectionLostOnce){
+                Materialize.Toast.removeAll();
+                Materialize.toast("Estamos de volta =)",5000);
+            }
+                
+        } else {
+            connectionLostOnce = true;
+            Materialize.toast("Você está desconectado.");
+        }
+    }); 
+    },2000)
+    
--- public/scripts/processanoticia.js
+++ public/scripts/processanoticia.js
@@ -22,43 +22,71 @@ function renderNoticia(dados){
         newsBit(campo.titulo, campo.descricao,campo.tipo,section);
     }
 }
-function pegaNoticia(callbackVariable){
-    var teste = $.getJSON("https://smdevasao.firebaseio.com/noticia.json");
-                    teste.done(function(data){
-                       callbackVariable.data = Object.entries(data);
-                        console.log(data);
-                        renderNoticia(callbackVariable.data);
-                    })
-                     .fail(function(jqxhr, textStatus, error){
-                      var result = "Requisicao falhou. " + textStatus + error;  
-                        console.log(result);
-                        callbackVariable.data = Object.entries(data);
-                        renderNoticia(callbackVariable.data);
-                    });
-}
 
 function pegaNoticiaFirebase(){
-    var section = document.getElementById("news");
-    section.innerHTML = "";
-    noticiaRef.orderByKey().limitToFirst(20).on("child_added", snap =>{
+    var collapse = document.getElementById("news");
+    var news = [];
+    collapse.innerHTML = "";
+    noticiaRef.orderByKey().limitToLast(20).on("child_added", snap =>{
         var titulo = snap.child("titulo").val();
         var desc = snap.child("descricao").val();
         var tipo = snap.child("tipo").val();
-        newsBit(titulo,desc,tipo,section);
+        news.push({titulo,desc,tipo});
+        newsBitMaterialize(titulo,desc,tipo,collapse);
+        
     });
+        
 }
 
-function newsBit(titulo, desc, tipo, destino) {
-    
-    var art = document.createElement("article");
-    var titl = document.createElement("h2");
-    titl.innerHTML = titulo + "<br>";
+function newsBitMaterialize(titulo, desc, tipo, destino) {
+    var container = document.createElement("li");
+
+    var header = document.createElement("div");
+    header.classList.add("collapsible-header");
+
+    var body = document.createElement("div");
+    body.classList.add("collapsible-body");
+
+    var icon = document.createElement("i");
+    icon.classList.add("material-icons");
+    icon.style.verticalAlign = "middle";
+
+    var titl = document.createElement("p");
+    titl.innerHTML = titulo;
+
     var descricao = document.createElement("p");
-    descricao.innerHTML = desc + "<br>";
-    var tp = document.createElement("p");
-    tp.innerHTML = "<b>Tipo:</b>" + tipo + "<br>";
-    art.appendChild(titl);
-    art.appendChild(descricao);
-    art.appendChild(tp);
-    destino.appendChild(art);
+    descricao.innerHTML = desc;
+
+    //saber qual icone usar
+    tipo = Number(tipo) || -1;
+    switch(tipo){
+        case 1:
+            //estagios
+            icon.innerHTML = "business_center";
+            break;
+        case 2:
+            //oportunidades
+            icon.innerHTML = "monetization_on";
+            break;
+        case 3:
+            //instituto
+            icon.innerHTML = "account_balance";
+            break;
+        case 4:
+            //eventos
+            icon.innerHTML = "event";
+            break;
+        default:
+            //nada :(
+            icon.innerHTML = "cancel";
+            break;
+    }
+
+    header.appendChild(icon);
+    header.appendChild(titl);
+    body.appendChild(descricao);
+    container.appendChild(header);
+    container.appendChild(body);
+    container.setAttribute("tabindex","0");
+    destino.insertBefore(container, destino.childNodes[0]);
 }
